17d5a730ec6f4a7213f003ee3632baa38a8126fa
Bez <bezmoradi@gmail.com>
2026-08-03 06:22:43 -0400

    chore: bump version to v1.1.0

--- internal/commands/constants.go
+++ internal/commands/constants.go
@@ -1,5 +1,5 @@
 package commands
 
-const VERSION = "v1.0.5"
-const UPDATE_MESSAGE = `Added arch boundaries.
+const VERSION = "v1.1.0"
+const UPDATE_MESSAGE = `Added Docker & CI generation, JWT auth, OpenAPI docs, DB migrations, OpenTelemetry & more.
 To support this project, please give it a ⭐`
